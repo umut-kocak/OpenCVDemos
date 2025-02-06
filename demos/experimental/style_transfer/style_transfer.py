e6400bc1a104efd0304ab64bf453a1e7c899c696
umut-kocak <umut.kocak@gmail.com>
2025-02-06 21:38:29 +0100

    Update experimental style transfer. Still not working properly.

--- demos/experimental/style_transfer/style_transfer.py
+++ demos/experimental/style_transfer/style_transfer.py
@@ -3,7 +3,8 @@
 from pathlib import Path
 import cv2
 
-from utils.base_module import BaseVideoDemo
+
+from utils.base_video_demo import BaseVideoDemo
 from utils.video_writer import VideoWriter
 import utils.image_styler as image_styler
 
@@ -14,10 +15,10 @@ class StyleTransferDemo(BaseVideoDemo):
         super().__init__()
 
         # Load a custom background image
-        _style_image_path = self.get_assets_folder() / self.settings.demo.style_image
-        self._style_image = cv2.imread(_style_image_path)
-        if self._style_image is None:
-            logging.error("No stlye image found: %s", _style_image_path)
+        #_style_image_path = self.get_assets_folder() / self.settings.demo.style_image
+        #self._style_image = cv2.imread(_style_image_path)
+        #if self._style_image is None:
+        #    logging.error("No stlye image found: %s", _style_image_path)
         #self._style_image = cv2.cvtColor(self._style_image, cv2.COLOR_BGR2RGB)
         #self._style_image = cv2.convertScaleAbs(self._style_image, alpha=0.8, beta=0)  # Reduce brightness
 
@@ -34,14 +35,15 @@ class StyleTransferDemo(BaseVideoDemo):
         """
         #styled_image = image_styler.style_transfer(frame.image, self._style_image)
         #styled_image = cv2.cvtColor(styled_image, cv2.COLOR_RGB2BGR)
-        styled_image = image_styler.fast_neural_style_transfer(frame.image, self.get_assets_folder()/ "models" / "rain_princess.pth")
+        #styled_image = image_styler.fast_neural_style_transfer(frame.image, self.get_assets_folder()/ "models" / "rain_princess.pth")
+        styled_image = image_styler.fast_neural_style_transfer(frame.image, self.get_asset_path( "models/rain_princess.pth") )
         #styled_image = frame.image
 
-        if self._output_index is not None:
-            output_path = self.get_output_folder() / ('stlyed-' + str(self._output_index) + '.jpg')
-            cv2.imwrite(output_path, styled_image)
-            self._output_index += 1
-        self._video_writer.write(styled_image)
+        #if self._output_index is not None:
+        #    output_path = self.get_output_folder() / ('stlyed-' + str(self._output_index) + '.jpg')
+        #    cv2.imwrite(output_path, styled_image)
+        #    self._output_index += 1
+        #self._video_writer.write(styled_image)
 
         frame.image = styled_image
         return frame
